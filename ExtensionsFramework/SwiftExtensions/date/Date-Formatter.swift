07460e8f66767aac461178f78c89ff4da66ebc59
Alexandru Bartis (Fortech) <alexandrub@fortech.ro>
2019-05-07 10:40:45 +0300

    Swift 5 update

--- ExtensionsFramework/SwiftExtensions/date/Date-Formatter.swift
+++ ExtensionsFramework/SwiftExtensions/date/Date-Formatter.swift
@@ -9,73 +9,73 @@
 import Foundation
 
 public enum ISO8601Type: String {
-  case year					= "yyyy"
-  case yearMonth				= "yyyy-MM"
-  case date					= "yyyy-MM-dd"
-  case hourMinute             = "HH:mm"
-  case time                   = "HH:mm:ss"
-  case dateTime				= "yyyy-MM-dd'T'HH:mmZZZZZ"
-  case full					= "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-  case extended				= "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    case year					= "yyyy"
+    case yearMonth				= "yyyy-MM"
+    case date					= "yyyy-MM-dd"
+    case hourMinute             = "HH:mm"
+    case time                   = "HH:mm:ss"
+    case dateTime				= "yyyy-MM-dd'T'HH:mmZZZZZ"
+    case full					= "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    case extended				= "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
 }
 
 public struct DateFormat {
-  public static let Default = "yyyy-MM-dd'T'HH:mm:ss"
-  public static let DateTimeShort = "ddMMYYHH:mm"
-  public static let TimeShort = "jj:mm"
-  public static let DayMonth = "EEEEdMMMM"
-  public static let LongDateTimeFormat = "EEEdMMMM jj:mm"
-  public static let Date = "EEEEdMMMMYYYY"
-  public static let DateShort = "EEEdMM"
+    public static let Default = "yyyy-MM-dd'T'HH:mm:ss"
+    public static let DateTimeShort = "ddMMYYHH:mm"
+    public static let TimeShort = "jj:mm"
+    public static let DayMonth = "EEEEdMMMM"
+    public static let LongDateTimeFormat = "EEEdMMMM jj:mm"
+    public static let Date = "EEEEdMMMMYYYY"
+    public static let DateShort = "EEEdMM"
 }
 
 public extension Date {
-  
-  public static func parseISO8601String(_ isoStringDate: Any?, isoType: ISO8601Type = .full) -> Date?  {
-    guard let isoString = isoStringDate as? String else { return nil }
-    let formatter = CustomDateFormatter.formatter
-    formatter.dateFormat = isoType.rawValue
-    formatter.calendar = CustomDateFormatter.gregorianCalendar
-    return formatter.date(from: isoString)
-  }
-  
-  public func toISO8601String(isoType iso8601type: ISO8601Type = .full) -> String? {
-    return toString(format: iso8601type.rawValue)
-  }
-  
-  public func toString(format dateFormat: String, calendar: Calendar = Calendar.current) -> String? {
-    let dateFormatter = CustomDateFormatter.formatter
-    dateFormatter.dateFormat = dateFormat
-    dateFormatter.timeZone = NSTimeZone.default
-    dateFormatter.calendar = calendar
-    return dateFormatter.string(from: self)
-  }
-  
-  public func toString(template templateFormat: String, locale: Locale = Locale.current, calendar: Calendar = Calendar.current) -> String? {
-    let dateFormatter = CustomDateFormatter.createDateFormatterFromTemplate(templateFormat, locale: locale)
-    dateFormatter.timeZone = NSTimeZone.default
-    dateFormatter.locale = locale
-    dateFormatter.calendar = calendar
-    return dateFormatter.string(from: self)
-  }
-  
+    
+    static func parseISO8601String(_ isoStringDate: Any?, isoType: ISO8601Type = .full) -> Date?  {
+        guard let isoString = isoStringDate as? String else { return nil }
+        let formatter = CustomDateFormatter.formatter
+        formatter.dateFormat = isoType.rawValue
+        formatter.calendar = CustomDateFormatter.gregorianCalendar
+        return formatter.date(from: isoString)
+    }
+    
+    func toISO8601String(isoType iso8601type: ISO8601Type = .full) -> String? {
+        return toString(format: iso8601type.rawValue)
+    }
+    
+    func toString(format dateFormat: String, calendar: Calendar = Calendar.current) -> String? {
+        let dateFormatter = CustomDateFormatter.formatter
+        dateFormatter.dateFormat = dateFormat
+        dateFormatter.timeZone = NSTimeZone.default
+        dateFormatter.calendar = calendar
+        return dateFormatter.string(from: self)
+    }
+    
+    func toString(template templateFormat: String, locale: Locale = Locale.current, calendar: Calendar = Calendar.current) -> String? {
+        let dateFormatter = CustomDateFormatter.createDateFormatterFromTemplate(templateFormat, locale: locale)
+        dateFormatter.timeZone = NSTimeZone.default
+        dateFormatter.locale = locale
+        dateFormatter.calendar = calendar
+        return dateFormatter.string(from: self)
+    }
+    
 }
 
 public class CustomDateFormatter {
-  
-  public static var gregorianCalendar: Calendar = {
-    return Calendar(identifier: .gregorian)
-  }()
-  
-  public static var formatter: Foundation.DateFormatter = {
-    let formatter = Foundation.DateFormatter()
-    return formatter
-  }()
-  
-  public static func createDateFormatterFromTemplate(_ dateTemplateFormat: String = DateFormat.Default, locale: Locale? = Locale.current) -> Foundation.DateFormatter {
-    let templateFromat = Foundation.DateFormatter.dateFormat(fromTemplate: dateTemplateFormat, options: 0, locale: locale)
-    formatter.dateFormat = templateFromat ?? dateTemplateFormat
-    return formatter
-  }
+    
+    public static var gregorianCalendar: Calendar = {
+        return Calendar(identifier: .gregorian)
+    }()
+    
+    public static var formatter: Foundation.DateFormatter = {
+        let formatter = Foundation.DateFormatter()
+        return formatter
+    }()
+    
+    public static func createDateFormatterFromTemplate(_ dateTemplateFormat: String = DateFormat.Default, locale: Locale? = Locale.current) -> Foundation.DateFormatter {
+        let templateFromat = Foundation.DateFormatter.dateFormat(fromTemplate: dateTemplateFormat, options: 0, locale: locale)
+        formatter.dateFormat = templateFromat ?? dateTemplateFormat
+        return formatter
+    }
 }
 
--- ExtensionsFramework/SwiftExtensions/date/Date-Utils.swift
+++ ExtensionsFramework/SwiftExtensions/date/Date-Utils.swift
@@ -10,49 +10,49 @@ import Foundation
 
 //MARK: Date utilities methods
 public extension Date {
-  
-  public init(date fromDate: Date, year: Int? = nil,
-              month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil,
-              nanosecond: Int? = nil, calendar: Calendar? = nil) {
     
-    let newComponents = DateComponents(
-      year: year ?? fromDate.year,
-      month: month ?? fromDate.month,
-      day: day ?? fromDate.day,
-      hour: hour ?? fromDate.hour,
-      minute: minute ?? fromDate.minute,
-      second: second ?? fromDate.second,
-      nanosecond: nanosecond ?? fromDate.nanosecond)
+    init(date fromDate: Date, year: Int? = nil,
+         month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil,
+         nanosecond: Int? = nil, calendar: Calendar? = nil) {
+        
+        let newComponents = DateComponents(
+            year: year ?? fromDate.year,
+            month: month ?? fromDate.month,
+            day: day ?? fromDate.day,
+            hour: hour ?? fromDate.hour,
+            minute: minute ?? fromDate.minute,
+            second: second ?? fromDate.second,
+            nanosecond: nanosecond ?? fromDate.nanosecond)
+        
+        let newCalendar = calendar ?? Calendar.current
+        let newDate = newCalendar.date(from: newComponents) ?? Date()
+        
+        self.init(timeIntervalSinceReferenceDate: newDate.timeIntervalSinceReferenceDate)
+    }
     
-    let newCalendar = calendar ?? Calendar.current
-    let newDate = newCalendar.date(from: newComponents) ?? Date()
+    func nearestDateWithMinuteInterval(_ minuteInterval: Int) -> Date {
+        let nextTimeSlotRatio = ceil(Float(minute) / Float(minuteInterval))
+        let nextMinute = (Int(nextTimeSlotRatio) * minuteInterval) % 60
+        
+        return Date(date: self, hour:nearestHour, minute: nextMinute, second: 0, nanosecond: 0)
+    }
     
-    self.init(timeIntervalSinceReferenceDate: newDate.timeIntervalSinceReferenceDate)
-  }
-  
-  public func nearestDateWithMinuteInterval(_ minuteInterval: Int) -> Date {
-    let nextTimeSlotRatio = ceil(Float(minute) / Float(minuteInterval))
-    let nextMinute = (Int(nextTimeSlotRatio) * minuteInterval) % 60
+    func isBetweeen(date date1: Date, andDate date2: Date) -> Bool {
+        return date1.compare(self).rawValue * self.compare(date2).rawValue >= 0
+    }
     
-    return Date(date: self, hour:nearestHour, minute: nextMinute, second: 0, nanosecond: 0)
-  }
-  
-  public func isBetweeen(date date1: Date, andDate date2: Date) -> Bool {
-    return date1.compare(self).rawValue * self.compare(date2).rawValue >= 0
-  }
-  
-  public func startOfDay(calendar: Calendar = Calendar.current) -> Date {
-    return calendar.startOfDay(for: self)
-  }
-  
-  public func endOfDay(calendar: Calendar = Calendar.current) -> Date {
-    let startOfDayDate = startOfDay()
-    var components = DateComponents()
-    components.day = 1
-    components.second = -1
-    return calendar.date(byAdding: components, to: startOfDayDate) ?? (startOfDayDate + 23.hours)
-  }
-  
+    func startOfDay(calendar: Calendar = Calendar.current) -> Date {
+        return calendar.startOfDay(for: self)
+    }
+    
+    func endOfDay(calendar: Calendar = Calendar.current) -> Date {
+        let startOfDayDate = startOfDay()
+        var components = DateComponents()
+        components.day = 1
+        components.second = -1
+        return calendar.date(byAdding: components, to: startOfDayDate) ?? (startOfDayDate + 23.hours)
+    }
+    
 }
 
 
